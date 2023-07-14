0d6e7441aef721cee85cae973ad752d1a0770315
Marcus Vinicius <marcustere28@gmail.com>
2023-07-14 10:51:06 -0300

    refactor customer event test

--- src/domain/@shared/event/customer/handler/sendEmailWhenCustomerAddressIsChanged.handler.ts
+++ src/domain/@shared/event/customer/handler/sendEmailWhenCustomerAddressIsChanged.handler.ts
@@ -1,10 +1,10 @@
 import EventHandlerInterface from '../../event.handler.interface'
-import CustomerCreatedEvent from '../customerCreated.event'
+import CustomerChangedAddressEvent from '../customerChangedAddress.event'
 
 
-export default class SendEmailWhenCustomerAddressIsChangedHandler implements EventHandlerInterface<CustomerCreatedEvent> {
+export default class SendEmailWhenCustomerAddressIsChangedHandler implements EventHandlerInterface<CustomerChangedAddressEvent> {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  handler(event: CustomerCreatedEvent) {
+  handler(event: CustomerChangedAddressEvent) {
     console.log(`Endereço do cliente: ${event.eventData.id}, ${event.eventData.name} alterado para: ${event.eventData.address}`)
   }
 }
